de915c9be08bfc7c3486e0d8fb8848e1dbde2004
Muhammad Ammar Cheema <muhammadammar7747@gmail.com>
2024-10-13 17:40:51 +0500

    Callbacks done

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -2,9 +2,11 @@ import { auth, signOut } from "@/auth";
 
 const SettingsPage = async () => {
     const session = await auth();
+
     return (
         <div>
             <h1>Settings</h1>
+            <h2>{session?.user.role}</h2>
             <p>{JSON.stringify(session)}</p>
             <form action = {async () => {
                 "use server";
--- auth.ts
+++ auth.ts
@@ -1,24 +1,49 @@
-import NextAuth from "next-auth"
+import NextAuth, { Session } from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 import authConfig from "./auth.config"
 import { db } from "./lib/db"
+import { getUserbyId } from "./data/user"
 
 const prisma = new PrismaClient()
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
     callbacks: {
-        async session({session, token}){
+        async signIn({ user }){
+            const existingUser = await getUserbyId(user.id);
+
+            if (!existingUser || !existingUser.emailVerified) {
+                return false;
+            }
+            return true;
+        },
+
+        async session({ session, token }: { session: Session & { user: { id?: string, role?: "ADMIN" | "SELLER" | "BUYER" } }, token: any }){
+            console.log("session", session, token);
+
             if(session.user && token.sub){
                 session.user.id = token.sub;
             }
+
+            if(session.user && token.role){
+                session.user.role = token.role;
+            }
             return session;
         },
+
         async jwt({token}){
+            if (!token.sub) {
+                return token;
+            }
+            const existingUser = await getUserbyId(token.sub);
+            if (!existingUser) {
+                return token;
+            }
+            token.role = existingUser.role;
             return token;
         }
     },
     adapter: PrismaAdapter(prisma),
     session: { strategy: "jwt" },
     ...authConfig,
-})
+});
